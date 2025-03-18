b25bb4338d1c976be5337496e148c32a74ea72f1
aghabidareh <aghabidareh@gmail.com>
2025-03-18 10:13:50 -0700

    Find Customer Referee

--- 2-Find Customer Referee.sql
+++ 2-Find Customer Referee.sql
@@ -1,4 +1,6 @@
-# Write your MySQL query statement below
-SELECT name FROM Customer
-            where referee_id != 2 or
+#
+Write your MySQL query statement below
+SELECT name
+FROM Customer
+where referee_id != 2 or
              referee_id is null
