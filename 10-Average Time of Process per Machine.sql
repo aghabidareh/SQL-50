23a25e9d866b4c69661fb6bd83d7403e4b1cb60a
aghabidareh <aghabidareh@gmail.com>
2025-03-18 10:42:19 -0700

    Average Time of Process per Machine

--- 10-Average Time of Process per Machine.sql
+++ 10-Average Time of Process per Machine.sql
@@ -1,8 +1,9 @@
-# Write your MySQL query statement below
-SELECT
-    ac1.machine_id,
-    ROUND(AVG(ac2.timestamp-ac1.timestamp),3) as processing_time
+#
+Write your MySQL query statement below
+SELECT ac1.machine_id,
+       ROUND(AVG(ac2.timestamp - ac1.timestamp), 3) as processing_time
 FROM Activity ac1
-JOIN Activity ac2 ON ac2.machine_id=ac1.machine_id
-WHERE ac1.activity_type='start' AND ac2.activity_type='end'
+         JOIN Activity ac2 ON ac2.machine_id = ac1.machine_id
+WHERE ac1.activity_type = 'start'
+  AND ac2.activity_type = 'end'
 GROUP BY ac1.machine_id
