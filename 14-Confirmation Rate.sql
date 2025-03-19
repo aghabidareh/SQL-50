8e283d5943da5a21d2fe71373f1288664e23548d
aghabidareh <aghabidareh@gmail.com>
2025-03-19 03:10:40 -0700

    Confirmation Rate

--- 14-Confirmation Rate.sql
+++ 14-Confirmation Rate.sql
@@ -1,10 +1,9 @@
-# Write your MySQL query statement below
-select
-    s.user_id,
-    IFNULL(round(AVG(c.action = 'confirmed'),2),0) as confirmation_rate
-from
-    Signups as s left outer join Confirmations as c
-on
-    s.user_id = c.user_id
-group by
-    s.user_id
+#
+Write your MySQL query statement below
+select s.user_id,
+       IFNULL(round(AVG(c.action = 'confirmed'), 2), 0) as confirmation_rate
+from Signups as s
+         left outer join Confirmations as c
+                         on
+                             s.user_id = c.user_id
+group by s.user_id
