cef52fd90cfb3325b1ab45657ef780f1b0e4c157
aghabidareh <aghabidareh@gmail.com>
2025-03-19 03:15:15 -0700

    Average Selling Price

--- 16-Average Selling Price.sql
+++ 16-Average Selling Price.sql
@@ -1,10 +1,11 @@
-# Write your MySQL query statement below
+#
+Write your MySQL query statement below
 SELECT P.product_id,
        CASE
            WHEN SUM(U.units) is null THEN 0
            ELSE ROUND(SUM(U.units * P.price) / SUM(U.units), 2)
-       END AS average_price
+           END AS average_price
 FROM Prices AS P
-LEFT JOIN UnitsSold AS U ON P.product_id = U.product_id and
-u.purchase_date BETWEEN start_date AND end_date
+         LEFT JOIN UnitsSold AS U ON P.product_id = U.product_id and
+                                     u.purchase_date BETWEEN start_date AND end_date
 GROUP BY P.product_id;
