6c05a915ff65a34473d5e3f0800cd2e3d6dab054
aghabidareh <aghabidareh@gmail.com>
2025-03-19 03:14:22 -0700

    Not Boring Movies

--- 15-Not Boring Movies.sql
+++ 15-Not Boring Movies.sql
@@ -1,4 +1,5 @@
-# Write your MySQL query statement below
+#
+Write your MySQL query statement below
 SELECT id, movie, description, rating
 FROM Cinema
 WHERE id % 2 != 0 and description != 'boring'
