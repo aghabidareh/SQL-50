6912c18c64e557d462315b72afe1f943c2ee9309
aghabidareh <aghabidareh@gmail.com>
2025-03-20 05:52:35 -0700

    Investments in 2016

---
+++ 42-Investments in 2016.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select round(sum(tiv_2016),2) as tiv_2016 from insurance where tiv_2015 in (select tiv_2015 from insurance group by tiv_2015 having count(tiv_2015) > 1) AND lat+lon not in (select lat+lon from insurance group by lat+lon having count(lat+lon) > 1)
