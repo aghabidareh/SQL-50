e918180b7087df848abc3b4f1b9e1028b9ce2ed0
aghabidareh <aghabidareh@gmail.com>
2025-03-20 05:35:02 -0700

    Biggest Single Number

--- 28-Biggest Single Number.sql
+++ 28-Biggest Single Number.sql
@@ -1,9 +1,8 @@
-# Write your MySQL query statement below
+#
+Write your MySQL query statement below
 SELECT MAX(num) AS num
 FROM MyNumbers
-WHERE num IN (
-    SELECT num
-    FROM MyNumbers
-    GROUP BY num
-    HAVING COUNT(num) = 1
-);
+WHERE num IN (SELECT num
+              FROM MyNumbers
+              GROUP BY num
+              HAVING COUNT(num) = 1);
