28c4842426a0aacc6a68397b2e1dca696ea2a9f6
aghabidareh <aghabidareh@gmail.com>
2025-03-20 05:49:01 -0700

    Movie Rating

--- 39-Movie Rating.sql
+++ 39-Movie Rating.sql
@@ -1,20 +1,22 @@
 #
 Write your MySQL query statement below
-(
-select name as results
-From Users inner join MovieRating using(user_id)
-group by user_id
-order by COUNT(rating) DESC,name
-limit 1
-)
-
-UNION all
-
-(
-select title as results
-From Movies inner join MovieRating using(movie_id)
-where MONTH(created_at)='02' and YEAR(created_at)='2020'
-group by title
-order by AVG(rating) DESC, title
-limit 1
-)
+SELECT name AS results FROM (
+	SELECT u.name, COUNT(u.name) AS count
+	FROM Users u
+	JOIN MovieRating m
+	ON u.user_id = m.user_id
+	GROUP BY u.user_id
+	ORDER BY count DESC,u.name ASC
+	LIMIT 1
+) AS temp
+UNION ALL
+SELECT title AS results FROM (
+    SELECT mo.title, AVG(m.rating) AS avg_rating
+    FROM Movies mo
+    JOIN Movierating m
+    ON m.movie_id = mo.movie_id
+    WHERE m.created_at BETWEEN '2020-02-01' AND '2020-02-28'
+    GROUP BY m.movie_id
+    ORDER BY avg_rating DESC, mo.title ASC
+    LIMIT 1
+) AS temp1
