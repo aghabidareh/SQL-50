e5845c4bec2f426a6934dc856b0fd982eff5fdfa
aghabidareh <aghabidareh@gmail.com>
2025-03-20 05:39:46 -0700

    Triangle Judgement

--- 32-Triangle Judgement.sql
+++ 32-Triangle Judgement.sql
@@ -1,8 +1,11 @@
-# Write your MySQL query statement below
-SELECT x, y, z,
+#
+Write your MySQL query statement below
+SELECT x,
+       y,
+       z,
        CASE
            WHEN (x + y > z) AND (x + z > y) AND (y + z > x)
-           THEN 'Yes'
+               THEN 'Yes'
            ELSE 'No'
-       END AS triangle
+           END AS triangle
 FROM Triangle;
