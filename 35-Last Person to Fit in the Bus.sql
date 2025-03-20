b31f375c07041fed7e618d097492c05353b45b70
aghabidareh <aghabidareh@gmail.com>
2025-03-20 05:42:26 -0700

    Last Person to Fit in the Bus

--- 35-Last Person to Fit in the Bus.sql
+++ 35-Last Person to Fit in the Bus.sql
@@ -1,4 +1,5 @@
-# Write your MySQL query statement below
+#
+Write your MySQL query statement below
 WITH CTE AS (
     SELECT person_name, turn,
            SUM(weight) OVER (ORDER BY turn) AS cumulative_weight
@@ -7,5 +8,4 @@ WITH CTE AS (
 SELECT person_name
 FROM CTE
 WHERE cumulative_weight <= 1000
-ORDER BY turn DESC
-LIMIT 1;
+ORDER BY turn DESC LIMIT 1;
