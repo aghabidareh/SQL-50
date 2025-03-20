f7f1b5616e38818cc31cc02f23e076e2ad5a6405
aghabidareh <aghabidareh@gmail.com>
2025-03-20 05:53:24 -0700

    Department Top Three Salaries

--- 43-Department Top Three Salaries.sql
+++ 43-Department Top Three Salaries.sql
@@ -1,4 +1,5 @@
-# Write your MySQL query statement below
+#
+Write your MySQL query statement below
 with cte as (
     SELECT departmentId as id,
            name as Employee,
@@ -6,6 +7,7 @@ with cte as (
            dense_rank() over(partition by departmentId order by salary DESC) as rk
     FROM Employee
 )
-SELECT d.name as Department, Employee, salary FROM cte
-JOIN Department d ON cte.id = d.id
+SELECT d.name as Department, Employee, salary
+FROM cte
+         JOIN Department d ON cte.id = d.id
 WHERE rk <= 3
