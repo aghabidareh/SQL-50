c65fd2c7239494ff354bf8c1771c9a9e6303b625
aghabidareh <aghabidareh@gmail.com>
2025-03-20 05:38:47 -0700

    Primary Department for Each Employee

--- 31-Primary Department for Each Employee.sql
+++ 31-Primary Department for Each Employee.sql
@@ -1,9 +1,10 @@
-# Write your MySQL query statement below
+#
+Write your MySQL query statement below
 select e.employee_id, e.department_id
 from Employee e
-join (select employee_id, count(department_id) count_dep
-from Employee
-group by employee_id) dop
-using(employee_id)
-where count_dep = 1 or
-primary_flag='Y';
+         join (select employee_id, count(department_id) count_dep
+               from Employee
+               group by employee_id) dop
+              using (employee_id)
+where count_dep = 1
+   or primary_flag = 'Y';
